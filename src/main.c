c96d5d940bf5db6040939e2a15f3d1ca536edfc8
Andrei Vlad <43170039+Stipl3x@users.noreply.github.com>
2019-09-06 14:25:46 +0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,1 +1,20 @@
+/*
+ * main.c
+ *
+ * Created: 06-Sep-19 02:10:49 PM
+ * Author : Stipl3x
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+   
+   
+    /* Replace with your application code */
+    while (1) 
+    {
+       
+    }
+}
 
