702cbc72268556b4e30211246412d2a4b04448a3
Andrei Vlad <43170039+Stipl3x@users.noreply.github.com>
2019-09-24 16:09:15 +0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -13,11 +13,17 @@
 int main(void)
 {
     USART0_init();
+    uint8_t u8Data = 0;
     
-    while (1) 
+    while(1)
     {
         USART0_TX_data('a');
         _delay_ms(1000);
+        u8Data = USART0_RX_data();
+        _delay_ms(1000);
+        USART0_TX_data(u8Data);
+        _delay_ms(1000);
+        USART0_TX_data('\n');
     } // end of main loop
     
     return 0;
