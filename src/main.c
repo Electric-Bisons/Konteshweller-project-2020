0b849e072dd0c83c6ee206906d3c8042e25a5f19
Andrei Vlad <43170039+Stipl3x@users.noreply.github.com>
2019-09-28 17:57:43 +0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -9,22 +9,39 @@
 #include <avr/interrupt.h>
 
 #include "headers/driver_usart.h"
+#include "headers/driver_timers.h"
+#include "headers/driver_hcsr04.h"
+
+volatile uint8_t vu8Flag = 0;
 
 int main(void)
 {
+    // enable global interrupt
+    sei();
+    
+    // init drivers
     USART0_init();
-    uint8_t u8Data = 0;
+    HCSR04_init();
+    
+    HCSR04_trigger();
     
     while(1)
     {
-        USART0_TX_data('a');
-        _delay_ms(1000);
-        u8Data = USART0_RX_data();
-        _delay_ms(1000);
-        USART0_TX_data(u8Data);
-        _delay_ms(1000);
-        USART0_TX_data('\n');
+        // execute only after measure is complete
+        if( vu8Flag )
+        {
+            vu8Flag = 0;
+            HCSR04_trigger();
+        }
     } // end of main loop
     
     return 0;
 } // end of main
+
+ISR(INT4_vect)
+{
+    // send cm value over USART0
+    USART0_TX_data(HCSR04_echo_cm());
+    _delay_ms(1000);
+    vu8Flag = 1;
+}
