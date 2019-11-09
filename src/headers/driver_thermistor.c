40b89f2069b5e2d64b16aea07cccf9595ab82276
Andrei Vlad <43170039+Stipl3x@users.noreply.github.com>
2019-11-09 17:14:55 +0200

    Update driver_thermistor.c

--- src/headers/driver_thermistor.c
+++ src/headers/driver_thermistor.c
@@ -13,10 +13,11 @@ uint16_t thermConv(uint16_t u16RawADC)
 {
     double d64Temperature = 0;
     
-    d64Temperature = log10(10000.0 * (1024.0 / u16RawADC - 1));
+    d64Temperature = log(10000.0 * (1024.0 / u16RawADC - 1));
     d64Temperature = 1 / (0.001129148 + (0.000234125 + 
                     (0.0000000876741 * d64Temperature * d64Temperature)) 
                     * d64Temperature);
+    
     // Convert Kelvin to Celsius
     d64Temperature -= 273.15;
     d64Temperature *= 10;
